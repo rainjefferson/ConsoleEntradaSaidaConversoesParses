1d1106ad826fca2705369290b06d9b08b94cd661
rainjefferson <117676799+rainjefferson@users.noreply.github.com>
2022-11-24 19:43:12 -0300

    Add project files.

---
+++ ConsoleEntradaSaidaDados/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEntradaSaidaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Entrada e saida de dados do teclado
+            // Metodo que lê um caracter digitado no console em codigo ascii
+            //int tecla = Console.Read();
+            //Console.WriteLine(tecla);
+
+            // Metodo que lê uma linha digitada e retorna em uma string
+            //string frase = Console.ReadLine();
+            //Console.WriteLine(frase);
+            #endregion
+
+            #region Conversão explicita de variaveis
+            ushort numero = 33000;
+            byte numero2 = (byte)numero;
+            Console.WriteLine(numero2);
+
+            char letra = (char)97;
+
+            Console.WriteLine(letra);
+            #endregion
+
+            #region Metodo parse
+            // Metodo parse converte a cadeia de represantação de caracteres string em um  numero equivalente
+
+            string txtNumero = "2022";
+            int ano = int.Parse(txtNumero);
+            Console.WriteLine(ano);
+            #endregion
+
+            #region Classe Converte
+
+            string textoconvertido = Convert.ToString(344.908f);
+            Console.WriteLine("textoconvertido: " + textoconvertido);
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
